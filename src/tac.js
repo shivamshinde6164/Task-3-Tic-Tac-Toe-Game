42ff4eee08c475d5dedd1fa3d940dfc5d62e20f5
Shivam Shinde <shivam57s6164@gmail.com>
2023-03-14 18:37:18 +0530

    Update tac.js

--- src/tac.js
+++ src/tac.js
@@ -1,63 +1,92 @@
-let playerSymbol = "X";
-let gameEnded = false
+const boxs=document.querySelectorAll('.box');
+const statusTxt=document.querySelector('#status');
+const btnRestart=document.querySelector('#restart');
+let x="<img src='images/x.png'>";
+let o="<img src='images/o.png'>";
 
-let winPos = [
-  [1, 2, 3], [4, 5, 6], 
-  [7, 8, 9], [1, 4, 7], 
-  [2, 5, 8], [3, 6, 9], 
-  [1, 5, 9], [3, 5, 7]
+const win=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ];
 
-for (let i = 1; i <= 9; i++) {
-  // Whenever a player clicks on a cell
-  document.getElementById(i.toString()).addEventListener(
-    "click", 
-    function() {
-      if (this.innerHTML === "" && !gameEnded) {
-        // Display either "X" or "O" in the cell, and style it
-        this.innerHTML = playerSymbol;
-        this.classList.add(playerSymbol.toLowerCase());
-        
-        // Check if a player has won
-        checkWin();
-        
-        // Swap the symbol to the other one for the next turn
-        if (playerSymbol === "X")
-          playerSymbol = "O"
-        else
-          playerSymbol = "X"
-      }
-    }
-  );
+let options=["","","","","","","","",""];
+let currentPlayer=x;
+let player="X";
+let running=false;
+init();
+
+function init(){
+  boxs.forEach(box=>box.addEventListener('click',boxClick));
+  btnRestart.addEventListener('click',restartGame);
+  statusTxt.textContent=`${player} Your Turn`;
+  running=true;
 }
 
-function checkWin() {
-  for (let i = 0; i < winPos.length; i++) {
-	if (
-	  document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
-	  document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
-	  document.getElementById(winPos[i][2]).innerHTML === playerSymbol
-	) {
-	  document.getElementById(winPos[i][0]).classList.add("win");
-	  document.getElementById(winPos[i][1]).classList.add("win");
-	  document.getElementById(winPos[i][2]).classList.add("win"); 
-	  gameEnded = true;
-	  setTimeout(function() {
-	    alert(playerSymbol + " wins!");
-	  }, 500);
-	}
+function boxClick(){
+  const index=this.dataset.index;
+  if(options[index]!="" || !running){
+    return;
   }
+  updateBox(this,index);
+  checkWinner();
 }
 
-document.getElementById("reset").addEventListener(
-  "click", 
-  function() {
-    for (let i = 1; i <= 9; i++) {
-      document.getElementById(i.toString()).innerHTML = "";
-      document.getElementById(i.toString()).classList.remove("x");
-      document.getElementById(i.toString()).classList.remove("o");
-      document.getElementById(i.toString()).classList.remove("win");
-      gameEnded = false;
+function updateBox(box,index){
+  options[index]=player;
+  box.innerHTML=currentPlayer;
+}
+
+function changePlayer(){
+    player=(player=='X') ? "O" :"X";
+    currentPlayer=(currentPlayer==x) ? o :x;
+    statusTxt.textContent=`${player} Your Turn`;
+}
+
+function checkWinner(){
+  let isWon=false;
+  for(let i=0;i<win.length;i++){
+    const condition=win[i]; //[0,1,2]
+    const box1=options[condition[0]]; //x
+    const box2=options[condition[1]]; //''
+    const box3=options[condition[2]]; //''
+    if(box1=="" || box2=="" || box3==""){
+      continue;
+    }
+    if(box1==box2 && box2==box3){
+      isWon=true;
+      boxs[condition[0]].classList.add('win');
+      boxs[condition[1]].classList.add('win');
+      boxs[condition[2]].classList.add('win');
     }
   }
-);
+
+  if(isWon){
+    statusTxt.textContent=`${player} Won..`;
+    running=false;
+  }else if(!options.includes("")){
+    statusTxt.textContent=`Game Draw..!`;
+    running=false;
+  }else{
+    changePlayer();
+  }
+
+}
+
+function restartGame(){
+  options=["","","","","","","","",""];
+  currentPlayer=x;
+  player="X";
+  running=true;
+  statusTxt.textContent=`${player} Your Turn`;
+
+  boxs.forEach(box=>{
+      box.innerHTML="";
+      box.classList.remove('win');
+  });
+}
